29b1dc083f51406491b9829520e43447050d1c08
joshhaller <jhal441@aucklanduni.ac.nz>
2017-10-07 15:31:05 +1300

    Added transition between clapping and cheering

--- Assets/ClapController.cs
+++ Assets/ClapController.cs
@@ -6,6 +6,8 @@ public class ClapController : MonoBehaviour {
 
     public BikeController bike;
     public AudioClip[] claps;
+    private bool clap = true;
+    private bool cheer = false;
 
     // Use this for initialization
     void Start () {
@@ -14,7 +16,9 @@ public class ClapController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        GameObject[] crowds = GameObject.FindGameObjectsWithTag("CrowdSound");
+        GameObject[] crowdSounds = GameObject.FindGameObjectsWithTag("CrowdSound");
+        GameObject[] crowds = GameObject.FindGameObjectsWithTag("Crowd");
+
 
         //Handle vertical movement. Priority is Bike > Keyboard
         float moveVertical = 0.0f;
@@ -27,92 +31,115 @@ public class ClapController : MonoBehaviour {
             moveVertical = Input.GetAxis("Vertical");
         }
 
-        if (moveVertical > 3.0f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        if (moveVertical > 1.0f && clap)
+        {
+            Debug.Log("Clap");
+            foreach (GameObject crowd in crowds)
+            {=
+                crowd.GetComponent<Animator>().SetBool("Clap", true);
+                crowd.GetComponent<Animator>().SetBool("Cheer", false);
+            }
+            clap = false;
+            cheer = true;
+        }
+        else if (moveVertical < 1.0f && cheer)
         {
             foreach (GameObject crowd in crowds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[10];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowd.GetComponent<Animator>().SetBool("Cheer", true);
+                crowd.GetComponent<Animator>().SetBool("Clap", false);
+            }
+            cheer = false;
+            clap = true;
+        }
+
+
+        if (moveVertical > 3.0f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
+        {
+            foreach (GameObject crowdSound in crowdSounds)
+            {
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[10];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }            
         }
-        else if (moveVertical > 2.83f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 2.83f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[9];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[9];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 2.66f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 2.66f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[8];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[8];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 2.17f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 2.17f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[7];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[7];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 2.0f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 2.0f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[6];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[6];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 1.83f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 1.83f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[5];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[5];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 1.66f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 1.66f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[4];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[4];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 1.5f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 1.5f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[3];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[3];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 1.33f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 1.33f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[2];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[2];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 1.17f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 1.17f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[1];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[1];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
-        else if (moveVertical > 1.0f && !crowds[5].GetComponents<AudioSource>()[1].isPlaying)
+        else if (moveVertical > 1.0f && !crowdSounds[5].GetComponents<AudioSource>()[1].isPlaying)
         {
-            foreach (GameObject crowd in crowds)
+            foreach (GameObject crowdSound in crowdSounds)
             {
-                crowd.GetComponents<AudioSource>()[1].clip = claps[0];
-                crowd.GetComponents<AudioSource>()[1].Play();
+                crowdSound.GetComponents<AudioSource>()[1].clip = claps[0];
+                crowdSound.GetComponents<AudioSource>()[1].Play();
             }
         }
     }
